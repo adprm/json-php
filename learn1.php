25842205fa35809e7b791874bd4147f4d7fbffba
adprm <aditiyaprmn00@gmail.com>
2020-08-15 11:46:55 +0700

    title

--- learn1.php
+++ learn1.php
@@ -28,6 +28,16 @@
       </div>
     </nav>
 
+    <div class="container">
+
+      <div class="row mt-3">
+        <div class="col">
+          <h1>All Menu</h1>
+        </div>
+      </div>
+
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
