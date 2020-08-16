5e7acd0d6777637ee51a65e78186fc0a2cb468ba
adprm <aditiyaprmn00@gmail.com>
2020-08-16 12:01:06 +0700

    loop card

--- learn1.php
+++ learn1.php
@@ -46,17 +46,20 @@ $menu = $menu["menu"];
       </div>
 
       <div class="row">
-        <div class="col-md-4">
-          <div class="card">
-            <img src="img/menu/bigmac.png" class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">Burger Bigmac</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <h5>Rp. 60.000,-</h5>
-              <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+      <!-- looping card -->
+        <?php foreach($menu as $m) : ?>
+          <div class="col-md-4">
+            <div class="card">
+              <img src="img/menu/bigmac.png" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Burger Bigmac</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5>Rp. 60.000,-</h5>
+                <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+              </div>
             </div>
           </div>
-        </div>
+        <?php endforeach; ?>
       </div>
 
     </div>
